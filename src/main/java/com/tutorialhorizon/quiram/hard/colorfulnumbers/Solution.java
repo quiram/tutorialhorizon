08a8ab6fac47fbb32c148b1527801ab3e4479e4b
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-03-27 15:55:28 -0700

    add solution for "Colourful Numbers"

---
+++ src/main/java/com/tutorialhorizon/quiram/hard/colorfulnumbers/Solution.java
@@ -0,0 +1,36 @@
+package com.tutorialhorizon.quiram.hard.colorfulnumbers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a href="https://tutorialhorizon.com/algorithms/colorful-numbers/">123. Colourful Numbers</a>
+ */
+class Solution {
+    static boolean isColorfulNumber(int i) {
+        final Set<Integer> set = new HashSet<>();
+        final String s = Integer.toString(i);
+        final int n = s.length();
+
+        for (int size = 1; size < n; size++) {
+            for (int j = 0; j < n - size + 1; j++) {
+                String s1 = s.substring(j, j + size);
+                final int m = multiply(s1);
+                if (set.contains(m)) {
+                    return false;
+                } else {
+                    set.add(m);
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static int multiply(String s) {
+        return Arrays.stream(s.split(""))
+                .map(Integer::parseInt)
+                .reduce(1, (a, b) -> a * b);
+    }
+}
---
+++ src/test/java/com/tutorialhorizon/quiram/hard/colorfulnumbers/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.tutorialhorizon.quiram.hard.colorfulnumbers;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "3245,true",
+            "123,false",
+            "326,false",
+            "1,true",
+            "567,true"
+    })
+    void isColorfulNumber(int n, boolean result) {
+        assertEquals(result, Solution.isColorfulNumber(n));
+    }
+}
